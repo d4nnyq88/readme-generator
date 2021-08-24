fb371d1829fa9e3b2d615cbd3d621a310cdb46fc
Captain America <82297346+d4nnyq88@users.noreply.github.com>
2021-08-23 22:52:39 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# readme-generator
+#Project 1
+  ## License:
+  MIT[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)  
+  ## Table of Contents:
+  [Description](#description)
+  [Installation](#installation)
+  [Usage](#usage)
+  [Contributing](#contributing)
+  [Tests](#tests)
+  [Questions](#questions)  
+  ## Description:
+  good
+  &nbsp;  
+  ## Installation:
+  what
+  &nbsp;  
+  ## Usage:
+  cool
+  &nbsp;  
+  ## Contributing:
+  add stuff
+  &nbsp;  
+  ## Tests:
+  testing
+  &nbsp;  
+   ## Questions:
+  If you have any questions you can reach me at danny.quigley88@gmail.com and visit my [GitHub](https://www.github.com/d4nnyq88).
+  
---
+++ index.js
@@ -0,0 +1,89 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generateREADME = (answers) =>
+  `#${answers.name}
+  ## License:
+  ${answers.license}  
+  ## Table of Contents:
+  [Description](#description)
+  [Installation](#installation)
+  [Usage](#usage)
+  [Contributing](#contributing)
+  [Tests](#tests)
+  [Questions](#questions)  
+  ## Description:
+  ${answers.description}
+  &nbsp;  
+  ## Installation:
+  ${answers.installation}
+  &nbsp;  
+  ## Usage:
+  ${answers.usage}
+  &nbsp;  
+  ## Contributing:
+  ${answers.contribution}
+  &nbsp;  
+  ## Tests:
+  ${answers.test}
+  &nbsp;  
+   ## Questions:
+  If you have any questions you can reach me at ${answers.email} and visit my [GitHub](https://www.github.com/${answers.github}).
+  `;
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Enter your project name.',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Enter your project description.',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Enter installation instructions.',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Enter usage information.',
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'Enter contribution guidelines.',
+    },
+    {
+      type: 'input',
+      name: 'test',
+      message: 'Enter test instructions.',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Enter test instructions.',
+      choices: ['BSD[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)','MIT[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)','GPL[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'],
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address.',
+    },
+  ])
+  .then((answers) => {
+    const readMeContent = generateREADME(answers);
+
+    fs.writeFile('README.md', readMeContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created ReadME.md!')
+    );
+  });
